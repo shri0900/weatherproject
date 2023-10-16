df2f3a41630e6769410afcd41f196d19c8d21c09
shri0900 <144302909+shri0900@users.noreply.github.com>
2023-10-16 21:46:29 +0530

    weathermaster

---
+++ weather.js
@@ -0,0 +1,26 @@
+function getWeather() {
+    const city = document.getElementById('city').value;
+
+    if (!city) {
+        alert('Please enter a city name.');
+        return;
+    }
+
+    fetch('http://localhost:8081/getweather', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ city: city }),
+        mode:('no-cors')
+    })
+    .then(response => console.log(response.body))
+    .then(data => {
+        document.getElementById('minTemp').innerText = `${data["Minimum Temprature"]} °C`;
+        document.getElementById('maxTemp').innerText = `${data["Maximum Temprature"]} °C`;
+    })
+    .catch(error => {
+        alert('Error fetching weather. Please try again.');
+        console.error('Error:', error);
+    });
+}
